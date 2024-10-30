a6e542d2bb84a0f0c74c37872f8cd5c71b807127
Lauriane <dmbagdjelauriane@gmail.com>
2024-10-30 19:29:49 +0000

    add files

---
+++ app.py
@@ -0,0 +1,20 @@
+import gradio as gr
+from rag_system import RAG  # Importer la classe RAG
+
+# Initialiser l'instance de RAG
+rag = RAG()
+
+# Fonction utilisée par l'interface Gradio
+def generate_response(prompt):
+    return rag.retrieve_response(prompt)
+
+# Définir l'interface Gradio
+interface = gr.Interface(
+    fn=generate_response,
+    inputs=gr.Textbox(label="Posez une question sur les formations au DIT"),
+    outputs=gr.Textbox(label="Réponse d'Aida"),
+    title="Aida - Agent conversationnel du DIT"
+)
+
+if __name__ == "__main__":
+    interface.launch()
